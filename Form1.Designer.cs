f36e8a6198dac5e8013c57f70207fbeb4316450d
marcio <marcio@DESKTOP-639HUNL>
2024-05-16 21:31:52 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace Urna_Eletronica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Urna = new System.Windows.Forms.PictureBox();
+            this.btOK = new System.Windows.Forms.Button();
+            this.bt3 = new System.Windows.Forms.Button();
+            this.bt9 = new System.Windows.Forms.Button();
+            this.bt7 = new System.Windows.Forms.Button();
+            this.bt6 = new System.Windows.Forms.Button();
+            this.bt5 = new System.Windows.Forms.Button();
+            this.nulo = new System.Windows.Forms.Button();
+            this.bt0 = new System.Windows.Forms.Button();
+            this.bt8 = new System.Windows.Forms.Button();
+            this.bt4 = new System.Windows.Forms.Button();
+            this.bt1 = new System.Windows.Forms.Button();
+            this.bt2 = new System.Windows.Forms.Button();
+            this.cancela = new System.Windows.Forms.Button();
+            this.visor = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Urna)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Urna
+            // 
+            this.Urna.Image = global::Urna_Eletronica.Properties.Resources.Design_sem_nome;
+            this.Urna.Location = new System.Drawing.Point(12, 12);
+            this.Urna.Name = "Urna";
+            this.Urna.Size = new System.Drawing.Size(776, 426);
+            this.Urna.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Urna.TabIndex = 0;
+            this.Urna.TabStop = false;
+            // 
+            // btOK
+            // 
+            this.btOK.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btOK.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btOK.Location = new System.Drawing.Point(690, 284);
+            this.btOK.Name = "btOK";
+            this.btOK.Size = new System.Drawing.Size(52, 32);
+            this.btOK.TabIndex = 1;
+            this.btOK.Text = "OK";
+            this.btOK.UseVisualStyleBackColor = false;
+            this.btOK.Click += new System.EventHandler(this.btOK_Click);
+            // 
+            // bt3
+            // 
+            this.bt3.BackColor = System.Drawing.Color.White;
+            this.bt3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.bt3.Location = new System.Drawing.Point(690, 129);
+            this.bt3.Name = "bt3";
+            this.bt3.Size = new System.Drawing.Size(44, 27);
+            this.bt3.TabIndex = 2;
+            this.bt3.Text = "3";
+            this.bt3.UseVisualStyleBackColor = false;
+            this.bt3.Click += new System.EventHandler(this.bt3_Click);
+            // 
+            // bt9
+            // 
+            this.bt9.BackColor = System.Drawing.Color.White;
+            this.bt9.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.bt9.Location = new System.Drawing.Point(690, 212);
+            this.bt9.Name = "bt9";
+            this.bt9.Size = new System.Drawing.Size(44, 27);
+            this.bt9.TabIndex = 3;
+            this.bt9.Text = "9";
+            this.bt9.UseVisualStyleBackColor = false;
+            this.bt9.Click += new System.EventHandler(this.bt9_Click);
+            // 
+            // bt7
+            // 
+            this.bt7.BackColor = System.Drawing.Color.White;
+            this.bt7.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.bt7.Location = new System.Drawing.Point(565, 212);
+            this.bt7.Name = "bt7";
+            this.bt7.Size = new System.Drawing.Size(44, 27);
+            this.bt7.TabIndex = 4;
+            this.bt7.Text = "7";
+            this.bt7.UseVisualStyleBackColor = false;
+            this.bt7.Click += new System.EventHandler(this.bt7_Click);
+            // 
+            // bt6
+            // 
+            this.bt6.BackColor = System.Drawing.Color.White;
+            this.bt6.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.bt6.Location = new System.Drawing.Point(690, 170);
+            this.bt6.Name = "bt6";
+            this.bt6.Size = new System.Drawing.Size(44, 27);
+            this.bt6.TabIndex = 5;
+            this.bt6.Text = "6";
+            this.bt6.UseVisualStyleBackColor = false;
+            this.bt6.Click += new System.EventHandler(this.bt6_Click);
+            // 
+            // bt5
+            // 
+            this.bt5.BackColor = System.Drawing.Color.White;
+            this.bt5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.bt5.Location = new System.Drawing.Point(627, 170);
+            this.bt5.Name = "bt5";
+            this.bt5.Size = new System.Drawing.Size(44, 27);
+            this.bt5.TabIndex = 6;
+            this.bt5.Text = "5";
+            this.bt5.UseVisualStyleBackColor = false;
+            this.bt5.Click += new System.EventHandler(this.bt5_Click);
+            // 
+            // nulo
+            // 
+            this.nulo.BackColor = System.Drawing.Color.White;
+            this.nulo.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.nulo.Location = new System.Drawing.Point(556, 293);
+            this.nulo.Name = "nulo";
+            this.nulo.Size = new System.Drawing.Size(53, 23);
+            this.nulo.TabIndex = 7;
+            this.nulo.Text = "Nulo";
+            this.nulo.UseVisualStyleBackColor = false;
+            this.nulo.Click += new System.EventHandler(this.nulo_Click);
+            // 
+            // bt0
+            // 
+            this.bt0.BackColor = System.Drawing.Color.White;
+            this.bt0.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.bt0.Location = new System.Drawing.Point(627, 251);
+            this.bt0.Name = "bt0";
+            this.bt0.Size = new System.Drawing.Size(44, 26);
+            this.bt0.TabIndex = 8;
+            this.bt0.Text = "0";
+            this.bt0.UseVisualStyleBackColor = false;
+            this.bt0.Click += new System.EventHandler(this.bt0_Click);
+            // 
+            // bt8
+            // 
+            this.bt8.BackColor = System.Drawing.Color.White;
+            this.bt8.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.bt8.Location = new System.Drawing.Point(629, 212);
+            this.bt8.Name = "bt8";
+            this.bt8.Size = new System.Drawing.Size(44, 27);
+            this.bt8.TabIndex = 10;
+            this.bt8.Text = "8";
+            this.bt8.UseVisualStyleBackColor = false;
+            this.bt8.Click += new System.EventHandler(this.bt8_Click);
+            // 
+            // bt4
+            // 
+            this.bt4.BackColor = System.Drawing.Color.White;
+            this.bt4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.bt4.Location = new System.Drawing.Point(565, 171);
+            this.bt4.Name = "bt4";
+            this.bt4.Size = new System.Drawing.Size(44, 27);
+            this.bt4.TabIndex = 11;
+            this.bt4.Text = "4";
+            this.bt4.UseVisualStyleBackColor = false;
+            this.bt4.Click += new System.EventHandler(this.bt4_Click);
+            // 
+            // bt1
+            // 
+            this.bt1.BackColor = System.Drawing.Color.White;
+            this.bt1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.bt1.Location = new System.Drawing.Point(567, 126);
+            this.bt1.Name = "bt1";
+            this.bt1.Size = new System.Drawing.Size(44, 27);
+            this.bt1.TabIndex = 12;
+            this.bt1.Text = "1";
+            this.bt1.UseVisualStyleBackColor = false;
+            this.bt1.Click += new System.EventHandler(this.bt1_Click);
+            // 
+            // bt2
+            // 
+            this.bt2.BackColor = System.Drawing.Color.White;
+            this.bt2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.bt2.Location = new System.Drawing.Point(629, 128);
+            this.bt2.Name = "bt2";
+            this.bt2.Size = new System.Drawing.Size(42, 27);
+            this.bt2.TabIndex = 13;
+            this.bt2.Text = "2";
+            this.bt2.UseVisualStyleBackColor = false;
+            this.bt2.Click += new System.EventHandler(this.bt2_Click);
+            // 
+            // cancela
+            // 
+            this.cancela.BackColor = System.Drawing.Color.Red;
+            this.cancela.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.cancela.Location = new System.Drawing.Point(618, 293);
+            this.cancela.Name = "cancela";
+            this.cancela.Size = new System.Drawing.Size(65, 23);
+            this.cancela.TabIndex = 14;
+            this.cancela.Text = "Cancela";
+            this.cancela.UseVisualStyleBackColor = false;
+            this.cancela.Click += new System.EventHandler(this.cancela_Click);
+            // 
+            // visor
+            // 
+            this.visor.Location = new System.Drawing.Point(49, 103);
+            this.visor.Multiline = true;
+            this.visor.Name = "visor";
+            this.visor.Size = new System.Drawing.Size(435, 188);
+            this.visor.TabIndex = 15;
+            this.visor.TextChanged += new System.EventHandler(this.visor_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.visor);
+            this.Controls.Add(this.cancela);
+            this.Controls.Add(this.bt2);
+            this.Controls.Add(this.bt1);
+            this.Controls.Add(this.bt4);
+            this.Controls.Add(this.bt8);
+            this.Controls.Add(this.bt0);
+            this.Controls.Add(this.nulo);
+            this.Controls.Add(this.bt5);
+            this.Controls.Add(this.bt6);
+            this.Controls.Add(this.bt7);
+            this.Controls.Add(this.bt9);
+            this.Controls.Add(this.bt3);
+            this.Controls.Add(this.btOK);
+            this.Controls.Add(this.Urna);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Urna)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Urna;
+        private System.Windows.Forms.Button btOK;
+        private System.Windows.Forms.Button bt3;
+        private System.Windows.Forms.Button bt9;
+        private System.Windows.Forms.Button bt7;
+        private System.Windows.Forms.Button bt6;
+        private System.Windows.Forms.Button bt5;
+        private System.Windows.Forms.Button nulo;
+        private System.Windows.Forms.Button bt0;
+        private System.Windows.Forms.Button bt8;
+        private System.Windows.Forms.Button bt4;
+        private System.Windows.Forms.Button bt1;
+        private System.Windows.Forms.Button bt2;
+        private System.Windows.Forms.Button cancela;
+        private System.Windows.Forms.TextBox visor;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Urna_Eletronica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void visor_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void bt1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void bt2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt0_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nulo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cancela_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btOK_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
